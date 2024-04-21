652e0f63bd7893ec42a0228f8faea00f2414515e
VitthalGund <vitthalgund30@gmail.com>
2024-04-22 03:09:30 +0530

    updated

--- webchats/settings.py
+++ webchats/settings.py
@@ -206,3 +206,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
 
 WSGI_APPLICATION = "webchats.wsgi.application"
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
