9b16592f0529540c1b39cb59766224ae8fbd774a
VitthalGund <vitthalgund30@gmail.com>
2024-04-22 02:32:02 +0530

    bug fixes

--- webchats/asgi.py
+++ webchats/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for learnSync project.
+ASGI config for webchats project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learnSync.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webchats.settings")
 
 application = get_asgi_application()
